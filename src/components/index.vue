eb4ad501593fed0b646506df62716a0b3d091d40
tzaiyg <tzaiyg@qq.com>
2020-11-03 22:28:53 +0800

    Initial commit

---
+++ src/components/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-breadcrumb separator="/">
+    <el-link :underline="false">危险链接</el-link>
+  </el-breadcrumb>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      activeName: 'first'
+    };
+  },
+  methods: {
+
+    }
+
+}
+</script>
+<style>
+.change{
+  color: #000;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import index from '@/components/index'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/index',
+      name: 'index',
+      component: index
+    },
+    {
+      path: '/chuxians',
+      name: 'chuxians',
+      component: chuxians
+    }
+  ]
+})
